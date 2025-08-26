187229032ea1ffcc5942eed507a46ad38dfb0015
Ahnaf Shahriar <unknownhacker400@gmail.com>
2025-08-26 16:15:10 +0600

    Update Linear search.cpp

--- Linear search.cpp
+++ Linear search.cpp
@@ -2,23 +2,6 @@
 
 using namespace std;
 
-void ascendsort(int arr[], int n){
-
-for(int i=0;i<n-1;i++){
-    for(int j=0;j<n-i-1;j++){
-        if(arr[j]>arr[j+1]){
-            int temp=arr[j];
-            arr[j]=arr[j+1];
-            arr[j+1]=temp;
-        }
-    }
-}
-
-
-
-}
-
-
 
 
 int main(){
@@ -39,13 +22,6 @@ for(int i=0;i<n;i++){
 
 cout<<endl;
 
-ascendsort(a,n);
-
-cout<<"The array is sorted in ascending order:     ";
-
-for(int i=0;i<n;i++){
-    cout<<a[i]<<" ";
-}
 
 cout<<endl;
 cout<<"Enter the number you want to search:     ";
@@ -53,14 +29,17 @@ cout<<"Enter the number you want to search:     ";
 int s;
 
 cin>>s;
-bool flag=true;
+int res=-1;
 
 for(int k=0;k<n;k++){
     if(a[k]==s){
-        cout<<"Found at index: "<<k;
-        flag=false;
+        res=k;
+        cout<<"Found at index: ";
     }
 
+
 }
+
+return res;
 
 }
